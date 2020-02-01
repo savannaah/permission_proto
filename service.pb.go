fb5d89ed99bd7940c145158c5816598ecc6410d7
saujanreshmi <saujan.thapa94@gmail.com>
2020-02-01 16:18:07 +1000

    removed relatedrecord permission

--- service.pb.go
+++ service.pb.go
@@ -118,6 +118,53 @@ func (m *ApplicationRequest) GetName() string {
 	return ""
 }
 
+type CreateRecordRequest struct {
+	Header               *RequestHeader `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
+	Name                 string         `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
+	XXX_unrecognized     []byte         `json:"-"`
+	XXX_sizecache        int32          `json:"-"`
+}
+
+func (m *CreateRecordRequest) Reset()         { *m = CreateRecordRequest{} }
+func (m *CreateRecordRequest) String() string { return proto.CompactTextString(m) }
+func (*CreateRecordRequest) ProtoMessage()    {}
+func (*CreateRecordRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a0b84a42fa06f626, []int{2}
+}
+
+func (m *CreateRecordRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_CreateRecordRequest.Unmarshal(m, b)
+}
+func (m *CreateRecordRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_CreateRecordRequest.Marshal(b, m, deterministic)
+}
+func (m *CreateRecordRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_CreateRecordRequest.Merge(m, src)
+}
+func (m *CreateRecordRequest) XXX_Size() int {
+	return xxx_messageInfo_CreateRecordRequest.Size(m)
+}
+func (m *CreateRecordRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_CreateRecordRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_CreateRecordRequest proto.InternalMessageInfo
+
+func (m *CreateRecordRequest) GetHeader() *RequestHeader {
+	if m != nil {
+		return m.Header
+	}
+	return nil
+}
+
+func (m *CreateRecordRequest) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
 type DataRequest struct {
 	Header               *RequestHeader `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
 	Name                 string         `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
@@ -131,7 +178,7 @@ func (m *DataRequest) Reset()         { *m = DataRequest{} }
 func (m *DataRequest) String() string { return proto.CompactTextString(m) }
 func (*DataRequest) ProtoMessage()    {}
 func (*DataRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a0b84a42fa06f626, []int{2}
+	return fileDescriptor_a0b84a42fa06f626, []int{3}
 }
 
 func (m *DataRequest) XXX_Unmarshal(b []byte) error {
@@ -171,61 +218,6 @@ func (m *DataRequest) GetId() int32 {
 		return m.Id
 	}
 	return 0
-}
-
-type RelatedRecordRequest struct {
-	Header               *RequestHeader `protobuf:"bytes,1,opt,name=header,proto3" json:"header,omitempty"`
-	Name                 string         `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	Application          string         `protobuf:"bytes,3,opt,name=application,proto3" json:"application,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
-	XXX_unrecognized     []byte         `json:"-"`
-	XXX_sizecache        int32          `json:"-"`
-}
-
-func (m *RelatedRecordRequest) Reset()         { *m = RelatedRecordRequest{} }
-func (m *RelatedRecordRequest) String() string { return proto.CompactTextString(m) }
-func (*RelatedRecordRequest) ProtoMessage()    {}
-func (*RelatedRecordRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a0b84a42fa06f626, []int{3}
-}
-
-func (m *RelatedRecordRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_RelatedRecordRequest.Unmarshal(m, b)
-}
-func (m *RelatedRecordRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_RelatedRecordRequest.Marshal(b, m, deterministic)
-}
-func (m *RelatedRecordRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_RelatedRecordRequest.Merge(m, src)
-}
-func (m *RelatedRecordRequest) XXX_Size() int {
-	return xxx_messageInfo_RelatedRecordRequest.Size(m)
-}
-func (m *RelatedRecordRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_RelatedRecordRequest.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_RelatedRecordRequest proto.InternalMessageInfo
-
-func (m *RelatedRecordRequest) GetHeader() *RequestHeader {
-	if m != nil {
-		return m.Header
-	}
-	return nil
-}
-
-func (m *RelatedRecordRequest) GetName() string {
-	if m != nil {
-		return m.Name
-	}
-	return ""
-}
-
-func (m *RelatedRecordRequest) GetApplication() string {
-	if m != nil {
-		return m.Application
-	}
-	return ""
 }
 
 type DashboardRequest struct {
@@ -325,8 +317,8 @@ func (m *PermissionResponse) GetMessage() string {
 func init() {
 	proto.RegisterType((*RequestHeader)(nil), "permissionproto.RequestHeader")
 	proto.RegisterType((*ApplicationRequest)(nil), "permissionproto.ApplicationRequest")
+	proto.RegisterType((*CreateRecordRequest)(nil), "permissionproto.CreateRecordRequest")
 	proto.RegisterType((*DataRequest)(nil), "permissionproto.DataRequest")
-	proto.RegisterType((*RelatedRecordRequest)(nil), "permissionproto.RelatedRecordRequest")
 	proto.RegisterType((*DashboardRequest)(nil), "permissionproto.DashboardRequest")
 	proto.RegisterType((*PermissionResponse)(nil), "permissionproto.PermissionResponse")
 }
@@ -334,30 +326,28 @@ func init() {
 func init() { proto.RegisterFile("service.proto", fileDescriptor_a0b84a42fa06f626) }
 
 var fileDescriptor_a0b84a42fa06f626 = []byte{
-	// 364 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x92, 0xc1, 0x4e, 0xf2, 0x40,
-	0x10, 0xc7, 0x53, 0xf8, 0xe0, 0x0b, 0x43, 0x50, 0x9c, 0x10, 0xd3, 0xa8, 0x21, 0x58, 0x63, 0xc2,
-	0x89, 0x03, 0x26, 0xde, 0x3c, 0x18, 0x38, 0x78, 0x32, 0x66, 0x4d, 0x3c, 0x19, 0xc3, 0xb2, 0x1d,
-	0x65, 0x03, 0x74, 0x6b, 0x67, 0xf1, 0x09, 0x7c, 0x5a, 0x9f, 0xc2, 0xb8, 0x14, 0x28, 0x96, 0x03,
-	0x07, 0x7b, 0x63, 0x86, 0xdd, 0xdf, 0x6f, 0xa6, 0xfb, 0x87, 0x06, 0x53, 0xf2, 0xa1, 0x15, 0xf5,
-	0xe2, 0xc4, 0x58, 0x83, 0x87, 0x31, 0x25, 0x73, 0xcd, 0xac, 0x4d, 0xe4, 0x1a, 0xc1, 0x0d, 0x34,
-	0x04, 0xbd, 0x2f, 0x88, 0xed, 0x1d, 0xc9, 0x90, 0x12, 0x6c, 0x41, 0xc5, 0x9a, 0x29, 0x45, 0xbe,
-	0xd7, 0xf1, 0xba, 0x35, 0xb1, 0x2c, 0xf0, 0x18, 0xaa, 0x52, 0x29, 0x62, 0xf6, 0x4b, 0xae, 0x9d,
-	0x56, 0xc1, 0x08, 0xf0, 0x36, 0x8e, 0x67, 0x5a, 0x49, 0xab, 0x4d, 0x94, 0x92, 0xf0, 0x1a, 0xaa,
-	0x13, 0x47, 0x73, 0x90, 0x7a, 0xbf, 0xdd, 0xfb, 0xa5, 0xed, 0x6d, 0x39, 0x45, 0x7a, 0x1a, 0x11,
-	0xfe, 0x45, 0x72, 0x4e, 0xa9, 0xc3, 0xfd, 0x0e, 0x34, 0xd4, 0x87, 0xd2, 0xca, 0x02, 0xd0, 0x78,
-	0x00, 0x25, 0x1d, 0xfa, 0xe5, 0x8e, 0xd7, 0xad, 0x88, 0x92, 0x0e, 0x83, 0x4f, 0x0f, 0x5a, 0x82,
-	0x66, 0xd2, 0x52, 0x28, 0x48, 0x99, 0x24, 0x2c, 0x42, 0xda, 0x81, 0xba, 0xdc, 0x7c, 0x31, 0x67,
-	0xaf, 0x89, 0x6c, 0x2b, 0x78, 0x81, 0xe6, 0x50, 0xf2, 0x64, 0x6c, 0x64, 0x21, 0x13, 0x04, 0xf7,
-	0x80, 0x0f, 0xeb, 0xcb, 0x82, 0x38, 0x36, 0x11, 0x13, 0xb6, 0x01, 0xd8, 0x4a, 0xbb, 0xe0, 0x81,
-	0x09, 0xc9, 0x59, 0x2a, 0x22, 0xd3, 0x41, 0x1f, 0xfe, 0xcf, 0x89, 0x59, 0xbe, 0xad, 0x60, 0xab,
-	0xb2, 0xff, 0x55, 0x06, 0xd8, 0x00, 0xf1, 0x19, 0x9a, 0x83, 0x09, 0xa9, 0x69, 0x26, 0x17, 0x78,
-	0x91, 0x1b, 0x37, 0x9f, 0x9a, 0x93, 0xfc, 0xa1, 0x1d, 0x63, 0x3e, 0xc1, 0x91, 0xa3, 0x0f, 0x12,
-	0x92, 0x96, 0x96, 0xcf, 0x84, 0x67, 0xb9, 0x9b, 0x99, 0xc8, 0xec, 0xc7, 0x1d, 0xc1, 0xa9, 0xe3,
-	0x3e, 0xc6, 0xa4, 0xf4, 0xab, 0x56, 0x3f, 0x80, 0xcc, 0x52, 0x7f, 0x62, 0x40, 0x67, 0xd8, 0x4a,
-	0x18, 0x5e, 0xee, 0x78, 0xc8, 0x7c, 0x02, 0xf7, 0x33, 0x8c, 0xc1, 0x77, 0x86, 0x75, 0x7a, 0x32,
-	0x0b, 0x9c, 0xef, 0x58, 0x60, 0x3b, 0x63, 0x7b, 0x39, 0xc6, 0x55, 0xf7, 0xcf, 0xd5, 0x77, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0x13, 0xef, 0x1e, 0x10, 0x58, 0x04, 0x00, 0x00,
+	// 334 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xb4, 0x92, 0x41, 0x4f, 0xf2, 0x40,
+	0x10, 0x86, 0x03, 0x7c, 0xf0, 0xc5, 0x21, 0x28, 0x8e, 0xc6, 0x34, 0x6a, 0x08, 0x56, 0x0f, 0x9c,
+	0x38, 0x60, 0xe2, 0xcd, 0x83, 0x81, 0x83, 0x27, 0x63, 0xd6, 0xab, 0x21, 0x0c, 0xdb, 0x51, 0x36,
+	0x48, 0xb7, 0x76, 0x16, 0x7f, 0x87, 0x3f, 0xd9, 0xb8, 0x16, 0x2c, 0x96, 0x03, 0x97, 0xde, 0x3a,
+	0xd3, 0xdd, 0xe7, 0xd9, 0xc9, 0xbc, 0xd0, 0x12, 0x4e, 0x3f, 0x8c, 0xe6, 0x7e, 0x92, 0x5a, 0x67,
+	0xf1, 0x20, 0xe1, 0x74, 0x61, 0x44, 0x8c, 0x8d, 0x7d, 0x23, 0xbc, 0x85, 0x96, 0xe2, 0xf7, 0x25,
+	0x8b, 0xbb, 0x67, 0x8a, 0x38, 0xc5, 0x63, 0xa8, 0x3b, 0x3b, 0xe7, 0x38, 0xa8, 0x74, 0x2b, 0xbd,
+	0x3d, 0xf5, 0x53, 0xe0, 0x09, 0x34, 0x48, 0x6b, 0x16, 0x09, 0xaa, 0xbe, 0x9d, 0x55, 0xe1, 0x04,
+	0xf0, 0x2e, 0x49, 0xde, 0x8c, 0x26, 0x67, 0x6c, 0x9c, 0x91, 0xf0, 0x06, 0x1a, 0x33, 0x4f, 0xf3,
+	0x90, 0xe6, 0xa0, 0xd3, 0xff, 0xa3, 0xed, 0x6f, 0x38, 0x55, 0x76, 0x1a, 0x11, 0xfe, 0xc5, 0xb4,
+	0xe0, 0xcc, 0xe1, 0xbf, 0x43, 0x82, 0xa3, 0x61, 0xca, 0xe4, 0x58, 0xb1, 0xb6, 0x69, 0x54, 0x86,
+	0xc2, 0x40, 0x73, 0x44, 0x8e, 0x4a, 0x40, 0xe3, 0x3e, 0x54, 0x4d, 0x14, 0xd4, 0xba, 0x95, 0x5e,
+	0x5d, 0x55, 0x4d, 0x14, 0x8e, 0xa1, 0x3d, 0x22, 0x99, 0x4d, 0x2d, 0x95, 0x33, 0xca, 0x03, 0xe0,
+	0xe3, 0xfa, 0xb2, 0x62, 0x49, 0x6c, 0x2c, 0x8c, 0x1d, 0x00, 0x71, 0xe4, 0x96, 0x32, 0xb4, 0x11,
+	0x7b, 0x4b, 0x5d, 0xe5, 0x3a, 0x18, 0xc0, 0xff, 0x05, 0x8b, 0xd0, 0xeb, 0x0a, 0xb6, 0x2a, 0x07,
+	0x9f, 0x35, 0x80, 0x5f, 0x20, 0x3e, 0x43, 0x7b, 0x38, 0x63, 0x3d, 0xcf, 0xed, 0x1c, 0x2f, 0x0b,
+	0xcf, 0x2d, 0x26, 0xe2, 0xb4, 0x78, 0x68, 0xcb, 0x33, 0xc7, 0x70, 0xe8, 0xe9, 0xf9, 0x7d, 0xe3,
+	0x55, 0xe1, 0xe6, 0x96, 0x38, 0xec, 0xc6, 0x9f, 0xc0, 0x99, 0xe7, 0x3f, 0x25, 0xac, 0xcd, 0x8b,
+	0xd1, 0xdf, 0x4b, 0xcf, 0x0d, 0x77, 0x5e, 0x60, 0xe4, 0x52, 0xb1, 0x9b, 0x61, 0x0a, 0x81, 0x37,
+	0xac, 0x77, 0x9c, 0xc3, 0x5f, 0x6c, 0xc1, 0x6f, 0x26, 0x61, 0x27, 0xc7, 0xb4, 0xe1, 0xff, 0x5c,
+	0x7f, 0x05, 0x00, 0x00, 0xff, 0xff, 0xf3, 0x72, 0x54, 0x15, 0xda, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -373,9 +363,8 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type PermissionClient interface {
 	CheckApplication(ctx context.Context, in *ApplicationRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
-	CheckCreateRecord(ctx context.Context, in *DataRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
+	CheckCreateRecord(ctx context.Context, in *CreateRecordRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
 	CheckSpecificDataPermission(ctx context.Context, in *DataRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
-	CheckRelatedRecord(ctx context.Context, in *RelatedRecordRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
 	CheckDashboardPermission(ctx context.Context, in *DashboardRequest, opts ...grpc.CallOption) (*PermissionResponse, error)
 }
 
@@ -396,7 +385,7 @@ func (c *permissionClient) CheckApplication(ctx context.Context, in *Application
 	return out, nil
 }
 
-func (c *permissionClient) CheckCreateRecord(ctx context.Context, in *DataRequest, opts ...grpc.CallOption) (*PermissionResponse, error) {
+func (c *permissionClient) CheckCreateRecord(ctx context.Context, in *CreateRecordRequest, opts ...grpc.CallOption) (*PermissionResponse, error) {
 	out := new(PermissionResponse)
 	err := c.cc.Invoke(ctx, "/permissionproto.Permission/CheckCreateRecord", in, out, opts...)
 	if err != nil {
@@ -408,15 +397,6 @@ func (c *permissionClient) CheckCreateRecord(ctx context.Context, in *DataReques
 func (c *permissionClient) CheckSpecificDataPermission(ctx context.Context, in *DataRequest, opts ...grpc.CallOption) (*PermissionResponse, error) {
 	out := new(PermissionResponse)
 	err := c.cc.Invoke(ctx, "/permissionproto.Permission/CheckSpecificDataPermission", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *permissionClient) CheckRelatedRecord(ctx context.Context, in *RelatedRecordRequest, opts ...grpc.CallOption) (*PermissionResponse, error) {
-	out := new(PermissionResponse)
-	err := c.cc.Invoke(ctx, "/permissionproto.Permission/CheckRelatedRecord", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -435,9 +415,8 @@ func (c *permissionClient) CheckDashboardPermission(ctx context.Context, in *Das
 // PermissionServer is the server API for Permission service.
 type PermissionServer interface {
 	CheckApplication(context.Context, *ApplicationRequest) (*PermissionResponse, error)
-	CheckCreateRecord(context.Context, *DataRequest) (*PermissionResponse, error)
+	CheckCreateRecord(context.Context, *CreateRecordRequest) (*PermissionResponse, error)
 	CheckSpecificDataPermission(context.Context, *DataRequest) (*PermissionResponse, error)
-	CheckRelatedRecord(context.Context, *RelatedRecordRequest) (*PermissionResponse, error)
 	CheckDashboardPermission(context.Context, *DashboardRequest) (*PermissionResponse, error)
 }
 
@@ -448,14 +427,11 @@ type UnimplementedPermissionServer struct {
 func (*UnimplementedPermissionServer) CheckApplication(ctx context.Context, req *ApplicationRequest) (*PermissionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckApplication not implemented")
 }
-func (*UnimplementedPermissionServer) CheckCreateRecord(ctx context.Context, req *DataRequest) (*PermissionResponse, error) {
+func (*UnimplementedPermissionServer) CheckCreateRecord(ctx context.Context, req *CreateRecordRequest) (*PermissionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckCreateRecord not implemented")
 }
 func (*UnimplementedPermissionServer) CheckSpecificDataPermission(ctx context.Context, req *DataRequest) (*PermissionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckSpecificDataPermission not implemented")
-}
-func (*UnimplementedPermissionServer) CheckRelatedRecord(ctx context.Context, req *RelatedRecordRequest) (*PermissionResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CheckRelatedRecord not implemented")
 }
 func (*UnimplementedPermissionServer) CheckDashboardPermission(ctx context.Context, req *DashboardRequest) (*PermissionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckDashboardPermission not implemented")
@@ -484,7 +460,7 @@ func _Permission_CheckApplication_Handler(srv interface{}, ctx context.Context, 
 }
 
 func _Permission_CheckCreateRecord_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(DataRequest)
+	in := new(CreateRecordRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -496,7 +472,7 @@ func _Permission_CheckCreateRecord_Handler(srv interface{}, ctx context.Context,
 		FullMethod: "/permissionproto.Permission/CheckCreateRecord",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PermissionServer).CheckCreateRecord(ctx, req.(*DataRequest))
+		return srv.(PermissionServer).CheckCreateRecord(ctx, req.(*CreateRecordRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -515,24 +491,6 @@ func _Permission_CheckSpecificDataPermission_Handler(srv interface{}, ctx contex
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(PermissionServer).CheckSpecificDataPermission(ctx, req.(*DataRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _Permission_CheckRelatedRecord_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(RelatedRecordRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(PermissionServer).CheckRelatedRecord(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/permissionproto.Permission/CheckRelatedRecord",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PermissionServer).CheckRelatedRecord(ctx, req.(*RelatedRecordRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -570,10 +528,6 @@ var _Permission_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CheckSpecificDataPermission",
 			Handler:    _Permission_CheckSpecificDataPermission_Handler,
-		},
-		{
-			MethodName: "CheckRelatedRecord",
-			Handler:    _Permission_CheckRelatedRecord_Handler,
 		},
 		{
 			MethodName: "CheckDashboardPermission",
